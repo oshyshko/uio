62a9364c14e794b9ec34a6fb107781052ad16713
oshyshko <oshyshko@gmail.com>
2017-09-12 18:28:05 -0700

    Snapshot 1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uio/uio "1.0"
+(defproject uio/uio "1.1-SNAPSHOT"
   :description "uio is a Clojure/Java library for accessing HDFS, S3, SFTP and other file systems via a single API."
 
   :repositories {"cloudera" "https://repository.cloudera.com/content/groups/cdh-releases-rcs"}
