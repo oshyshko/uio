d3fa6e598749ac8a21ea54808e20b01c22241d40
oshyshko <oshyshko@gmail.com>
2017-10-16 20:32:06 -0700

    Bump dep versions

--- project.clj
+++ project.clj
@@ -8,18 +8,20 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
 
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.201"] ; s3
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.213"] ; s3
                  [org.apache.httpcomponents/httpclient "4.5.3"] ; (needed by `aws-java-sdk-s3`)
 
                  [com.jcraft/jsch "0.1.54"]                 ; sftp
                  [com.jcraft/jzlib "1.1.3"]                 ; (needed by `jsch`)
 
-                 [org.apache.hadoop/hadoop-common "2.8.0"   ; hdfs
+                 [org.apache.hadoop/hadoop-common "2.8.1"   ; hdfs
                   :exclusions [org.apache.httpcomponents/httpcore]] ; conflicts with `aws-java-sdk-s3`
-                 [org.apache.hadoop/hadoop-hdfs "2.8.0"]
+                 [org.apache.hadoop/hadoop-hdfs "2.8.1"]
 
-                 [org.apache.commons/commons-compress "1.12"] ; bzip2, xz
-                 [org.tukaani/xz "1.6"]]                    ; xz (needed by `commons-compress`)
+                 [org.apache.commons/commons-compress "1.14"] ; bzip2, xz
+                 [org.tukaani/xz "1.6"]                     ; xz (needed by `commons-compress`)
+
+                 [org.clojure/tools.cli "0.3.5"]]           ; main
 
   ; TODO not working. Find a way to exclude .java files from JARs, but have 1 source dir for Java/Clojure
   ;:jar-exclusions [#".*\.java"]
