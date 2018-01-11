f90c44963053b3d2e72e7c36de9d1c2b11726d38
oshyshko <oshyshko@gmail.com>
2018-01-11 14:09:37 -0800

    Rollback midje 1.9.1 to 1.8.3

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
   :javac-options ["-source" "1.8"
                   "-target" "1.8"]
 
-  :profiles {:dev {:dependencies [[midje "1.9.1"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
                    :plugins      [[lein-midje "3.2.1"]]}}
 
   :aot [uio.main.main]
