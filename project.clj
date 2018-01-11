575a8f701c524f24d78b56494b285e5574bc1235
oshyshko <oshyshko@gmail.com>
2018-01-11 14:02:53 -0800

    Bump deps

--- project.clj
+++ project.clj
@@ -8,19 +8,19 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
 
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.213"] ; s3
-                 [com.amazonaws/aws-java-sdk-sts "1.11.213"] ; s3 with roles
-                 [org.apache.httpcomponents/httpclient "4.5.3"] ; (needed by `aws-java-sdk-s3`)
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.261"] ; s3
+                 [com.amazonaws/aws-java-sdk-sts "1.11.261"] ; s3 with roles
+                 [org.apache.httpcomponents/httpclient "4.5.4"] ; (needed by `aws-java-sdk-s3`)
 
                  [com.jcraft/jsch "0.1.54"]                 ; sftp
                  [com.jcraft/jzlib "1.1.3"]                 ; (needed by `jsch`)
 
-                 [org.apache.hadoop/hadoop-common "2.8.1"   ; hdfs
+                 [org.apache.hadoop/hadoop-common "2.8.1"   ; hdfs -- 3.0.0 is there, but doesn't have HTTPS repo
                   :exclusions [org.apache.httpcomponents/httpcore]] ; conflicts with `aws-java-sdk-s3`
                  [org.apache.hadoop/hadoop-hdfs "2.8.1"]
 
-                 [org.apache.commons/commons-compress "1.14"] ; bzip2, xz
-                 [org.tukaani/xz "1.6"]                     ; xz (needed by `commons-compress`)
+                 [org.apache.commons/commons-compress "1.15"] ; bzip2, xz
+                 [org.tukaani/xz "1.8"]                     ; xz (needed by `commons-compress`)
 
                  [org.clojure/tools.cli "0.3.5"]]           ; main
 
@@ -31,7 +31,7 @@
   :javac-options ["-source" "1.8"
                   "-target" "1.8"]
 
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+  :profiles {:dev {:dependencies [[midje "1.9.1"]]
                    :plugins      [[lein-midje "3.2.1"]]}}
 
   :aot [uio.main.main]
@@ -39,7 +39,7 @@
 
   :uberjar-name "uio.jar"
   :bin {:name "uio"}
-  
+
   :plugins [[lein-bin "0.3.4"]]
 
   ; A trick to prevent IntelliJ from resetting compiler/module version to "1.5"
